6011c25ee80266feb083b5016c3e947e8b1c299f
tungnguyen741 <tung.nguyen21098@gmail.com>
2020-04-01 13:55:35 +0700

    'lesson_2'

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const pug = require('pug');
+const app = express();
 
-app.get('/', function(request, reponse){
-	reponse.send('<button>click</button>');
+
+app.set('view engine', 'pug');
+app.set('views', 'C:\\xampp\\htdocs\\demo_ExpressJs\\demo-express\\views');
+
+app.get('/', (req, res) => {
+    res.render('index',
+    	users = [
+    		{name:'Tung', age: 15},
+    		{name:'Phuong', age: 15},
+    		{name:'Tam', age: 15},
+    		{name:'Hoa', age: 15},	
+    	])
 });
 
-app.listen(3000, function(){
-	console.log('ok fine');
+app.listen(8000, (req, res) => {
+    console.log('ok fine');
 });
+ 
